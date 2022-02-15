a1d0887664a102280d9782b3f0529470683423f4
Artur Almeida <arturoalmeidafreire@gmail.com>
2022-02-15 14:40:10 -0300

    Adiciona suporte a teclado

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const buttons   = Array.from(document.querySelectorAll('button')),
-      clear     = buttons[0],
-      backspace = buttons[1],
-      divide    = buttons[2],
-      multiply  = buttons[6],
-      some      = buttons[10],
-      subtract  = buttons[14],
-      equals    = buttons[17];
+      clear     = buttons[0].textContent,
+      backspace = buttons[1].textContent,
+      divide    = buttons[2].textContent,
+      multiply  = buttons[6].textContent,
+      some      = buttons[10].textContent,
+      subtract  = buttons[14].textContent,
+      equals    = buttons[17].textContent;
 
 const resultDisplay = document.querySelector('#result');
 
@@ -13,42 +13,51 @@ let currentValue = '',
     operator;
 
 buttons.map(button => {
-    button.addEventListener('click', (e) => {
-        switch (e.target) {
-            case clear:
-                currentValue = '';
-                operator = undefined;
-                break;
-            case backspace:
-                if(currentValue[currentValue.length - 1] == operator) {
-                    operator = undefined;
-                }
-                currentValue = currentValue.slice(0, -1);
-                break;
-
-            case some:
-            case subtract:
-            case multiply:
-            case divide:
-                if(!operator) {
-                    operator = e.target.textContent;
-                    currentValue += e.target.textContent;
-                }
-                break;
-
-            case equals: 
-                if(operator) {
-                    currentValue = operation(currentValue, operator);
-                    operator = undefined;
-                }
-                break;
-            default:
-                currentValue += e.target.textContent;
-                break;
-            }
-        addToDisplay(currentValue);
-    });
+    button.addEventListener('click', (e) => targetButton(e.target.textContent));
 });
+
+document.addEventListener('keydown', (e) => targetButton(e.key));
+
+function targetButton (event) {
+    switch (event) {
+        case clear:
+            currentValue = '';
+            operator = undefined;
+            break;
+        case 'Backspace':
+        case backspace:
+            if(currentValue[currentValue.length - 1] == operator) {
+                operator = undefined;
+            }
+            currentValue = currentValue.slice(0, -1);
+            break;
+            
+        case 'Enter':
+        case equals: 
+            if(operator) {
+                currentValue = operation(currentValue, operator);
+                operator = undefined;
+            }
+            break;
+
+        case some:
+        case subtract:
+        case multiply:
+        case divide:
+            if(!operator) {
+                operator = event;
+                currentValue += event;
+            }
+            break;
+    
+        case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '0': 
+        case '.':
+            currentValue += event;
+            break;
+        }
+    addToDisplay(currentValue);
+
+}
 
 function addToDisplay (content) {
     resultDisplay.textContent = content ? content : '0';
@@ -56,22 +65,22 @@ function addToDisplay (content) {
 
 function operation (string, operator) {
     const index = string.indexOf(operator);
-    const val1 = parseInt(string.substring(0, index));
-    const val2 = parseInt(string.substring(index + 1, string.length));
+    const val1 = parseFloat(string.substring(0, index));
+    const val2 = parseFloat(string.substring(index + 1, string.length));
 
     let result;
 
     switch (operator) {
-        case some.textContent:
+        case some:
             result = val1 + val2;
             break;
-        case subtract.textContent:
+        case subtract:
             result = val1 - val2;
             break;
-        case multiply.textContent:
+        case multiply:
             result = val1 * val2;
             break;
-        case divide.textContent:
+        case divide:
             result = val1 / val2;
             break;
     }
